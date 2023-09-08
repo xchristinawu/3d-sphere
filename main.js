f9af9a5cab79db3387d357b0472577159fb956ff
Christina Wu <xchristinawu@gmail.com>
2023-09-08 00:18:34 -0700

    sphere rerenders proportionally after window resize

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three"
+import "./style.css"
 
 // Scene
 const scene = new THREE.Scene();
@@ -16,14 +17,41 @@ const light = new THREE.PointLight(0xffffff, 150, 100);
 light.position.set(0, 10, 10);
 scene.add(light);
 
+
+// Sizes
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
 // Camera
-const camera = new THREE.PerspectiveCamera(45, 800 / 600);
+const camera = new THREE.PerspectiveCamera(45, sizes.width / sizes.height, 0.1, 100);
 camera.position.z = 20;
 scene.add(camera);
 
 // Renderer
 const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({canvas});
-renderer.setSize(800, 600);
+renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
+
+// Resize canvas immediately when window resized
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(sizes.width, sizes.height);
+})
+
+const loop = () => {
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(loop);
+}
+
+loop();
 
