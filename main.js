6049c4bead1a582304916a70a9413df4db70dcaa
Christina Wu <xchristinawu@gmail.com>
2023-09-08 16:36:22 -0700

    add color change on mousedown

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ const scene = new THREE.Scene();
 const geometry = new THREE.SphereGeometry(3, 64, 64);
 const material = new THREE.MeshStandardMaterial({
   color: "#00ff83",
+  roughness: 0.5,
 })
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
@@ -72,3 +73,28 @@ const tl = gsap.timeline({defaults: {duration:1}});
 tl.fromTo(mesh.scale, {z:0, x:0, y:0}, {z:1, x:1, y:1});
 tl.fromTo("nav", {y:"-100%"}, {y:"0%"});
 tl.fromTo(".title", {opacity:0}, {opacity:1});
+
+// Mouse animation color
+let mouseDown = false;
+let rgb = [12, 23, 55];
+window.addEventListener("mousedown", () => (mouseDown = true));
+window.addEventListener("mouseup", () => (mouseDown = false));
+
+window.addEventListener("mousemove", (e) => {
+  if (mouseDown) {
+    rgb = [
+      Math.round((e.pageX / sizes.width * 255)), // gives value between 0 and 255 as you move the mouse from left to right
+      Math.round((e.pageY / sizes.height * 255)), // gives value between 0 and 255 as you move the mouse from top to bottom
+      150 // default value for blue
+    ]
+
+    // Animate
+    let newColor = new THREE.Color(`rgb(${rgb.join(",")})`); // template literal = rgb(0, 100, 150);
+    // can't pass rgb[0] to mesh.material.color. needs a Three.js object to work
+    gsap.to(mesh.material.color, {
+      r:newColor.r, 
+      g:newColor.g, 
+      b:newColor.b
+    });
+  }
+})
